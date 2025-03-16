20908991b5349a040bb425afbfc7ebc25574aafc
Anand Lunawat <aplunawat@gmail.com>
2025-03-16 23:49:44 +0530

    Fixed UI

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="font-rubik">
+  <div class="font-rubik text-[#100B21]">
     <!-- Hero Section with Background -->
     <div class="relative flex flex-col h-96">
       <!-- Background Image -->
       <div class="absolute inset-0">
-        <img src="../assets/bg.jpg" class="z-50 object-cover w-full h-full" />
+        <img src="../assets/bg.jpg" class="z-50 object-fill w-full h-full md:object-cover" />
       </div>
-      <div class="absolute inset-0 bg-[#0000008a]"></div>
+      <div class="absolute inset-0 bg-[#0000006d]"></div>
 
       <!-- Overlay (Optional for better readability) -->
 
       <!-- Navbar -->
       <nav class="relative z-10 flex items-center justify-between p-4 text-white">
-        <h1 class="text-4xl font-extrabold text-[#1D153A]">Xploro</h1>
+        <h1 class="font-extrabold text-white md:text-4xl">Xploro</h1>
         <!-- <input type="text" placeholder="Search destination..." class="w-full p-2 text-black rounded md:w-1/2"> -->
-        <button class="px-4 py-2 font-semibold text-[20px] text-white bg-[#1d153a93] rounded">Login or Create Account</button>
+        <button class="px-4 py-2 font-semibold md:text-[20px] text-white bg-[#1d153a93] rounded">Login or Create Account</button>
       </nav>
 
       <!-- Hero Section -->
@@ -26,11 +26,11 @@
 
     <!-- Trending Destinations -->
     <section class="p-4 bg-white md:p-8">
-      <h3 class="mb-4 text-2xl font-semibold text-red-600">Trending Destinations</h3>
+      <h3 class="mb-4 text-2xl font-semibold">Trending Destinations</h3>
       <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4">
         <div v-for="(destination, index) in destinations" :key="index" class="p-4 bg-gray-100 rounded shadow-lg">
           <img :src="destination.image" class="object-cover w-full h-40 rounded" />
-          <h4 class="mt-2 text-lg font-semibold text-red-600">{{ destination.name }}</h4>
+          <h4 class="mt-2 text-lg font-semibold">{{ destination.name }}</h4>
         </div>
       </div>
     </section>
